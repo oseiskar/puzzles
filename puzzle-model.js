d85f92f08789a700c545b4a943cd11eac4b02a83
Otto Seiskari <otto.seiskari@gmail.com>
2015-12-25 20:54:09 +0200

    playable game

--- puzzle-model.js
+++ puzzle-model.js
@@ -10,7 +10,7 @@ function PuzzleModel() {
 
   function Piece(id, shape, color, initial_position) {
     this.id = id;
-    this.shape = shape;
+    this.shape = shape.map(function (p) { return { x: p[0], y: p[1] }; });
     this.color = color;
     this.position = {
       x: initial_position[0],
@@ -41,4 +41,84 @@ function PuzzleModel() {
   definePiece([[1,1],[1,0],[0,1]], 'white', [
     [1,0], [4,0]
   ]);
+
+  var board = new PuzzleModel.Board(this);
+
+  this.getMoves = function(piece) {
+    return board.getMoves(piece);
+  };
+
+  this.canMove = function(piece) {
+    return this.getMoves(piece).length > 0;
+  };
+
+  this.movePiece = function(piece, dx, dy) {
+    if (!board.canMove(piece,dx,dy)) throw("Illegal move");
+    piece.position.x += dx;
+    piece.position.y += dy;
+  };
 }
+
+PuzzleModel.Board = function (puzzle_model) {
+
+  function emptyBoard() {
+    var board = [];
+    for (var i=0; i<puzzle_model.width*puzzle_model.height; ++i)
+      board.push(null);
+    return board;
+  }
+
+  function placeBlock(board, x, y, piece) {
+    board[puzzle_model.width*y + x] = piece;
+  }
+
+  function getBlock(board, x, y, piece) {
+    return board[puzzle_model.width*y + x];
+  }
+
+  function placePiece(board, piece) {
+    for (var i in piece.shape) {
+      var x = piece.position.x + piece.shape[i].x;
+      var y = piece.position.y + piece.shape[i].y;
+      placeBlock(board,x,y,piece);
+    }
+  }
+
+  function canPlace(board, piece, dx, dy) {
+    for (var i in piece.shape) {
+      var x = piece.position.x + piece.shape[i].x + dx;
+      var y = piece.position.y + piece.shape[i].y + dy;
+      var block = getBlock(board,x,y);
+      if (x < 0 || x >= puzzle_model.width) return false;
+      if (y < 0 || y >= puzzle_model.height) return false;
+      if (block !== null && block !== piece) return false;
+    }
+    return true;
+  }
+
+  function fillBoard() {
+    var board = emptyBoard();
+    for (var i in puzzle_model.pieces) {
+      placePiece(board, puzzle_model.pieces[i]);
+    }
+    return board;
+  }
+
+  this.getMoves = function(piece) {
+    var board = fillBoard();
+    var moves = [];
+    for (var dx=-1; dx<=1; dx++) {
+      for (var dy=-1; dy<=1; dy++) {
+        if (Math.abs(dx)+Math.abs(dy) === 1 && canPlace(board, piece, dx, dy)) {
+          moves.push({x: dx, y: dy});
+        }
+      }
+    }
+    return moves;
+  };
+
+  this.canMove = function(piece, dx, dy) {
+    var board = fillBoard();
+    return canPlace(board, piece, dx, dy);
+  };
+};
--- puzzle-view.js
+++ puzzle-view.js
@@ -4,38 +4,71 @@
 
 function PuzzleView() {
 
-    var view = this;
-    this.model = new PuzzleModel();
+  this.selected_piece_group = null;
+  this.selected_piece = null;
+  this.prev_move = null;
 
-    this.d3root = d3.select('#puzzle')
-        .attr('viewBox', '0 0 '+this.model.width+' '+this.model.height)
-        .append('g');
+  var view = this;
+  this.model = new PuzzleModel();
 
-    this.d3root.append('rect')
-      .attr('x', 0).attr('y', 0)
-      .attr('width', this.model.width)
-      .attr('height', this.model.height)
-      .attr('class', 'board');
+  this.d3root = d3.select('#puzzle')
+    .attr('viewBox', '0 0 '+this.model.width+' '+this.model.height)
+    .append('g');
 
-    this.render();
+  this.d3root.append('rect')
+    .attr('x', 0).attr('y', 0)
+    .attr('width', this.model.width)
+    .attr('height', this.model.height)
+    .attr('class', 'board')
+    .on('click', function() { view.onBoardClick(); });
+
+  this.render();
 }
 
 PuzzleView.prototype.render = function () {
-    var view = this;
 
-    var pieces = this.d3root.selectAll('g.piece')
-      .data(this.model.pieces);
+  var pieces = this.d3root.selectAll('g.piece')
+    .data(this.model.pieces);
 
-    var new_pieces = pieces
-      .enter()
-      .append('g')
-      .attr('class', 'piece');
+  var new_pieces = pieces
+    .enter()
+    .append('g')
+    .attr('class', 'piece');
 
-    this.renderPieces(new_pieces);
+  this.renderPieces(new_pieces);
 
-    pieces.attr('transform', function(piece) {
-        return 'translate(' + piece.position.x + ',' + piece.position.y + ')';
-      });
+  pieces.attr('transform', function(piece) {
+    return 'translate(' + piece.position.x + ',' + piece.position.y + ')';
+  });
+};
+
+PuzzleView.prototype.onPieceClick = function (piece_group) {
+
+  if (this.selected_piece_group !== null) {
+    this.selected_piece_group.classed('selected', false);
+  }
+  this.selected_piece_group = piece_group;
+  piece_group.classed('selected', true);
+
+  this.selected_piece = piece_group.datum();
+};
+
+PuzzleView.prototype.onBoardClick = function () {
+  if (this.selected_piece === null) return;
+
+  var moves = this.model.getMoves(this.selected_piece);
+
+  if (moves.length === 0) return;
+
+  var move = moves[0];
+  if (moves.length > 1 && this.prev_move !== null &&
+    this.prev_move.x === -move.x && this.prev_move.y == -move.y)
+    move = moves[1];
+
+  this.prev_move = move;
+
+  this.model.movePiece(this.selected_piece, move.x, move.y);
+  this.render();
 };
 
 PuzzleView.prototype.renderPieces = function(d3_selector) {
@@ -47,10 +80,10 @@ PuzzleView.prototype.renderPieces = function(d3_selector) {
       all_pieces.push(cur);
       var prev = piece_shape[piece_shape.length-1];
       if (i > 0) prev = piece_shape[i-1];
-      var dx = cur[0]-prev[0];
-      var dy = cur[1]-prev[1];
+      var dx = cur.x-prev.x;
+      var dy = cur.y-prev.y;
       if (dx === 0 || dy === 0) {
-        all_pieces.push([prev[0]+dx*0.5, prev[1]+dy*0.5]);
+        all_pieces.push({x: prev.x+dx*0.5, y: prev.y+dy*0.5});
       }
     }
     return all_pieces;
@@ -58,15 +91,20 @@ PuzzleView.prototype.renderPieces = function(d3_selector) {
 
   var PADDING = 0.04;
 
+  var view = this;
+
   d3_selector
     .selectAll('rect')
     .data(function (piece) { return generateMiddlePieces(piece.shape); })
     .enter()
     .append('rect')
     .attr('width', 1-PADDING*2).attr('height', 1-PADDING*2)
-    .attr('x', function(shape) { return shape[0]+PADDING; })
-    .attr('y', function(shape) { return shape[1]+PADDING; })
-    .attr('class', 'block-base');
+    .attr('x', function(block) { return block.x+PADDING; })
+    .attr('y', function(block) { return block.y+PADDING; })
+    .attr('class', 'block-base')
+    .on('click', function() {
+      view.onPieceClick(d3.select(this.parentNode));
+    });
 
   d3_selector
     .selectAll('circle')
@@ -74,9 +112,12 @@ PuzzleView.prototype.renderPieces = function(d3_selector) {
     .enter()
     .append('circle')
     .attr('r', 0.2)
-    .attr('cx', function(shape) { return shape[0]+0.5; })
-    .attr('cy', function(shape) { return shape[1]+0.5; })
+    .attr('cx', function(block) { return block.x+0.5; })
+    .attr('cy', function(block) { return block.y+0.5; })
     .attr('fill', function(shape) {
       return d3.select(this.parentNode).datum().color;
+    })
+    .on('click', function() {
+      view.onPieceClick(d3.select(this.parentNode));
     });
 };
